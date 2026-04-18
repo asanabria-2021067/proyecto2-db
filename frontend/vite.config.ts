0a48d05d0636437de697fefe27111117852cec9d
Angel Sanabria <as1945228@gmail.com>
2026-04-18 01:14:08 -0600

    feat: add components.json for component configuration; update tsconfig files for path mapping; integrate tailwindcss in vite config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,12 +3,14 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+    tailwindcss(),
   ],
   resolve: {
     alias: {
@@ -19,6 +21,10 @@ export default defineConfig({
     host: '0.0.0.0',
     proxy: {
       '/api': {
+        target: 'http://backend:3000',
+        changeOrigin: true,
+      },
+      '/uploads': {
         target: 'http://backend:3000',
         changeOrigin: true,
       },
