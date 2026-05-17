0936ed296a12fb526f89ddd2b05df5f88c3ab0a0
Angel Sanabria <as1945228@gmail.com>
2026-05-17 03:32:45 -0600

    feat: initialize project documentation and backend database configuration files

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -3,7 +3,7 @@ import pg from 'pg';
 const pool = new pg.Pool({
 	host: process.env['DB_HOST'] ?? 'localhost',
 	port: Number(process.env['DB_PORT'] ?? 5432),
-	user: process.env['DB_USER'] ?? 'proy2',
+	user: process.env['DB_USER'] ?? 'proy3',
 	password: process.env['DB_PASSWORD'] ?? 'secret',
 	database: process.env['DB_NAME'] ?? 'tienda_libros',
 });
